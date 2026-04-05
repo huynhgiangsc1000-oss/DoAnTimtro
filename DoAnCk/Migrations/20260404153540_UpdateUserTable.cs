56b8c7587e58986f5a12e2387b69989543b3592a
Tuong Le Khoa Cuong <cuong@example.com>
2026-04-05 10:04:48 +0700

    Update user table migration

--- DoAnCk/Migrations/20260404153540_UpdateUserTable.cs
+++ DoAnCk/Migrations/20260404153540_UpdateUserTable.cs
@@ -8,10 +8,17 @@ namespace DoAnCk.Migrations
     public partial class UpdateUserTable : Migration
     {
         /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
+       
+            protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            // Ví dụ: Thêm một cột mới vào bảng Users
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
         }
+        
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
